86b68d70a7d5fe571444804089c8704864ee1213
curtisfranks <curtisfranks@4888ea36-3ec0-b3d7-c8c2-f119c123729f>
2012-06-29 04:11:44 +0000

    Added the difficulty level to the menu options as a Listpreference.

--- trunk/Team2Project3AndroidReversi/Team2Reversi/gen/team2/reversi/R.java
+++ trunk/Team2Project3AndroidReversi/Team2Reversi/gen/team2/reversi/R.java
@@ -35,15 +35,19 @@ public final class R {
     public static final class string {
         public static final int app_name=0x7f060000;
         public static final int confirm_title=0x7f060009;
+        public static final int easy_difficulty=0x7f060013;
         public static final int game_finished=0x7f06000d;
+        public static final int hard_difficulty=0x7f060015;
         public static final int is_droid_opponent=0x7f06000e;
         public static final int is_droid_opponent_description=0x7f06000f;
+        public static final int medium_difficulty=0x7f060014;
         public static final int new_game_msg=0x7f06000c;
         public static final int no_caption=0x7f06000b;
         public static final int p1=0x7f060001;
         public static final int p2=0x7f060002;
         public static final int restart_label=0x7f060006;
         public static final int restart_shortcut=0x7f060007;
+        public static final int select_difficulty=0x7f060012;
         public static final int settings_label=0x7f060003;
         public static final int settings_shortcut=0x7f060005;
         public static final int show_hints=0x7f060004;
